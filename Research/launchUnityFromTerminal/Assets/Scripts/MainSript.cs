fdaa04d4f9c9c58a688415c3c2520e8c8d127911
Pablo Alejos <alejosrocks@gmail.com>
2017-05-18 12:03:40 +0200

    Mini aplicacion que permite lanzar unity desde terminal

---
+++ Research/launchUnityFromTerminal/Assets/Scripts/MainSript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSript : MonoBehaviour
+{
+
+    String[] arguments;
+    int n = 0;
+
+    private void Start()
+    {
+       arguments = Environment.GetCommandLineArgs();
+    }
+
+    void Update()
+    {
+        if (n < 10)
+        {
+            WriteFile.AddRow(arguments[1]);
+            n++;
+        }else
+        {
+            Application.Quit();
+        }
+        
+    }
+}
---
+++ Research/launchUnityFromTerminal/pythonLauncher.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("test.exe 111")
+os.system("test.exe 222")
+os.system("test.exe 333")
+
