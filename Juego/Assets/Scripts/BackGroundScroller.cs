c328d65fcc737fab50e7076be11f320ded2ffeac
Pablo Alejos <alejosrocks@gmail.com>
2017-06-27 11:38:36 +0200

    27 jun

---
+++ Juego/Assets/Scripts/BackGroundScroller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundScroller : MonoBehaviour {
+
+    MeshRenderer mr;
+    Material mat;
+    public float speed;
+
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        mat = mr.material;
+    }
+
+
+	void Update () {
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.y = Time.time / speed;
+
+        mat.mainTextureOffset = offset;
+	
+	}
+}
---
+++ Juego/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    public void StartGame()
+    {
+        StartCoroutine(ChangeLevel());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator ChangeLevel()
+    {
+        float fadeTime = GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(1);
+    }
+
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
