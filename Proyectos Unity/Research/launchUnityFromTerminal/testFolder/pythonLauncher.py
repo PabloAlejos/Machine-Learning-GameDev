d6351e9431d7eabd46e63393447eeb90624b7dad
PabloAlejos <alejosrocks@gmail.com>
2017-07-02 19:13:56 +0200

    Actualizada estructura de carpetas

--- Proyectos Unity/Research/launchUnityFromTerminal/testFolder/pythonLauncher.py
+++
@@ -1,9 +0,0 @@
-import os
-import pandas as pd
-import time
-import numpy as np
-
-
-#os.system("..\\test.exe")
-scores = pd.read_csv("myData.csv", sep=',', header=None)
-print(np.mean(scores.values))
